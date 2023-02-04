2bd90286b9670e0580dd24039f5794c6437d81db
cmclaudet <cmclaudet@gmail.com>
2023-02-04 18:52:51 +0100

    Walk animation

--- Assets/tmp/PlayerMove.cs
+++ Assets/tmp/PlayerMove.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerMove : MonoBehaviour
-{
+public class PlayerMove : MonoBehaviour {
+	private static readonly int IsWalkingTrigger = Animator.StringToHash("isWalking");
+
 	[SerializeField]
 	private float walkSpeed = 10f;
 
 	[SerializeField]
 	private SpriteRenderer sprite;
+
+	[SerializeField]
+	private Animator animator;
 
     private Vector2 moveInput;
 	private Rigidbody rb;
@@ -29,13 +33,16 @@ public class PlayerMove : MonoBehaviour
 	{
 		Vector3 velocity = new Vector3(moveInput.x * walkSpeed, rb.velocity.y, moveInput.y * walkSpeed);
 		rb.velocity = transform.TransformDirection(velocity);
+
+		animator.SetBool(IsWalkingTrigger, Mathf.Abs(moveInput.x) > 0.01f);
+
 		if (velocity.x < 0)
 		{
 			sprite.flipX = true;
 		} else if (velocity.x > 0) {
-            sprite.flipX = false;
-        }
+        sprite.flipX = false;
     }
+	}
 
 	void OnMove(InputValue value)
 	{
